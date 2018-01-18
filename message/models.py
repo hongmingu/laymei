797f1c633ea62ed3efdf93d6349f78e09e5ba6de
hongmingu <gmh397@nate.com>
2018-01-18 09:21:26 +0900

    5

--- message/models.py
+++ message/models.py
@@ -1,3 +1,43 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.conf import settings
+from renoauth.models import *
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class Message(models.Model):
+    dump_text = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    _from = models.ForeignKey(UserExtension)
+    public = models.BooleanField(default=False)
+    recipients_list = models.BooleanField(default=False)
+    open = models.BooleanField(default=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        if self.public is True:
+            if self.has_list is False:
+                to = 'all'
+            else:
+                to = 'impossible'
+        else:
+            if self.has_list is False:
+                to = 'friends'
+            else:
+                to = 'recipients'
+        return "from: %s //to: %s //text : %s" % (self._from.usersubusername.username,
+                                                  to, self.dump_text)
+
+
+@python_2_unicode_compatible
+class MessageRecipientsList(models.Model):
+    dump_text = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "dump_text: %s" % self.dump_text
+
+
--- talk/models.py
+++ talk/models.py
@@ -1,3 +1,53 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import User
+from django.conf import settings
+from renoauth.models import *
+from message.models import *
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class TalkExtension(models.Model):
+    dump_text = models.CharField(max_length=10)
+
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    user = models.OneToOneField(UserExtension)
+
+
+@python_2_unicode_compatible
+class TalkRoom(models.Model):
+    talk_extension = models.ForeignKey(TalkExtension)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    _with = models.ForeignKey(UserExtension)
+
+
+@python_2_unicode_compatible
+class TalkClose(models.Model):
+    talk_room = models.OneToOneField(TalkRoom)
+
+
+@python_2_unicode_compatible
+class Talk(models.Model):
+    message = models.ForeignKey(Message, null=True, blank=True)
+    dump_text = models.CharField(max_length=20)
+    created = models.DateTimeField(auto_now_add=True)
+    talk_room = models.ForeignKey(TalkRoom)
+    _from = models.ForeignKey(UserExtension)
+
+    def __str__(self):
+        if self.public is True:
+            if self.whole is True:
+                to = 'all'
+            else:
+                to = 'impossible'
+        else:
+            if self.whole is True:
+                to = 'friends'
+            else:
+                to = 'recipients'
+        return "from: %s //to: %s //text : %s" % (self._from.usersubusername.username,
+                                                  to, self.dump_text)
